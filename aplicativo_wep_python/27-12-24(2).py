c96f70ba6f28fbdfca2dcff088365a72dae7abe0
Yuri-Antonio-Santos-Fernandes <yuriafer@gmail.com>
2025-03-17 11:39:05 -0300

    Update 27-12-24(2).py

--- aplicativo_wep_python/27-12-24(2).py
+++ aplicativo_wep_python/27-12-24(2).py
@@ -1,7 +1,7 @@
 import sys
 from streamlit .web import cli as stcli
 
-sys.argv = ["streamli", "run", "27-12-24.py"]
+sys.argv = ["streamlit", "run", "27-12-24.py"]
 sys.exit(stcli.main())
 
 #executor do app web
